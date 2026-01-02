800c7898bee9f09de970d8d0445caa7f43162bb8
Kube-ZEN: Zero-Effort KuberNetes <zen@kube-zen.io>
2026-01-02 16:16:23 -0500

    fix: remove all unused imports from test files
    - Remove unused imports from benchmark_test.go and evaluate_policies_test.go
    - All tests now compile and pass successfully

--- pkg/controller/benchmark_test.go
+++ pkg/controller/benchmark_test.go
@@ -25,7 +25,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 
 	sdklog "github.com/kube-zen/zen-sdk/pkg/logging"
 )
